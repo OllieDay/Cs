a85cf27cd9a76ae9e93007dced7b59bff455fa1d
Ollie Day <OllieDay@users.noreply.github.com>
2018-04-09 08:28:05 +0100

    Fix Linux installation by using bash instead of sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
 	runtime=osx-x64
@@ -6,5 +6,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	runtime=linux-x64
 fi
 
-dotnet build Cs/Cs.csproj -o /usr/local/Cs -c Release -r $runtime
-ln -s /usr/local/Cs/Cs /usr/local/bin/cs
+output="/usr/local/Cs"
+
+dotnet build Cs/Cs.csproj -o $output -c Release -r $runtime
+chmod +x $output/Cs
+ln -s $output/Cs /usr/local/bin/cs
